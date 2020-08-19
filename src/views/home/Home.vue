030116567d5d05d89a83ff7411a2c478a37c1643
xjjabb <lh2110738476@163.com>
2020-08-19 21:27:51 +0800

    首页导航完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,6 +2,7 @@
   <div id="home">
     <HomeNavBar></HomeNavBar>
     <HomeSwiper :banners="banners"></HomeSwiper>
+    <HomeRecommend :recommends="recommends"></HomeRecommend>
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 //首页子组件
 import HomeNavBar from './children/HomeNavBar.vue';
 import HomeSwiper from './children/HomeSwiper.vue';
+import HomeRecommend from './children/HomeRecommend.vue';
 
 //请求
 import {getHomeMultdata} from 'network/home.js';
@@ -21,13 +23,14 @@ export default {
       }
     },
     components: {
-      HomeNavBar,//首页导航
+      HomeNavBar,//首页顶部
       HomeSwiper,//首页轮播图
+      HomeRecommend,//首页导航
     },
     created(){
       getHomeMultdata().then(res=>{
         this.banners=res.data.data.banner.list;
-        this.recommends=res.data.data.recommend;
+        this.recommends=res.data.data.recommend.list;
       })
     }
 }
---
+++ src/views/home/children/HomeRecommend.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="recommend">
+      <ul>
+          <li v-for="(item,index) in recommends" :key="index">
+              <a :href="item.link">
+                  <img :src="item.image" alt="">
+                  <span>{{item.title}}</span>
+              </a>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    props: {
+        recommends: {
+            type: Array,
+            default(){
+                return {}
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #recommend {
+        padding: 10px 0 15px;
+        border-bottom: 6px solid #eee;
+    } 
+    #recommend ul {
+        display: flex;
+        width: 100%;
+    }
+    #recommend ul li {
+        flex: 20%;
+    }
+    #recommend ul li img {
+        width: 65px;
+        height: 80px;
+    }
+    #recommend ul li a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    #recommend ul a span {
+        margin-top: 10px;
+    }
+</style>
