53ab0c4e8b72a13e68087421c49509cac7caa100
xjjabb <lh2110738476@163.com>
2020-08-20 22:59:29 +0800

    详情页轮播图 完成

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="detail">
     <DetailNavBar></DetailNavBar>
+    <DetailSwiper :banners="banners"></DetailSwiper>
   </div>
 </template>
 
 <script>
 //详情页子组件
 import DetailNavBar from './children/DetailNavBar.vue';
-
+import DetailSwiper from './children/DetailSwiper.vue';
 //请求
 import {getDetail} from 'network/detail.js';
 export default {
@@ -15,10 +16,12 @@ export default {
     data(){
       return {
         iid: null,//商品id
+        banners: [],//详情页轮播图
       }
     },
     components: {
-      DetailNavBar
+      DetailNavBar,//详情页顶部
+      DetailSwiper,//详情页轮播图
     },
     created(){
       //保存iid
@@ -27,6 +30,8 @@ export default {
       //请求数据
       getDetail(this.iid).then(res=>{
         console.log(res);
+        //轮播图
+        this.banners=res.data.result.itemInfo.topImages;
       })
     }
 }
---
+++ src/views/detail/children/DetailSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <Swiper>
+      <SwiperItem v-for="(item,index) in banners" :key="index">
+          <img :src="item">
+      </SwiperItem>
+  </Swiper>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from 'components/common/swiper/index.js';
+export default {
+    name: 'DetailSwiper',
+    components: {
+        Swiper,
+        SwiperItem
+    },
+    props: {
+        banners: {
+            type: Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    img {
+        height: 350px;
+    }
+</style>
